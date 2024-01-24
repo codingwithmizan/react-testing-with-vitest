520f9a02ba93c27448c90d39f8be54563cba6f3b
codingwithmizan <mizan.samb@gmail.com>
2024-01-25 00:03:18 +0600

    lint stage 1

--- src/components/Application.tsx
+++ src/components/Application.tsx
@@ -1,12 +1,14 @@
 /** @format */
 
 const Application = () => {
-	console.log(x);
-	
-	const x = 90
+	// console.log(x);
+
+	// const x = 90
 	return (
 		<form>
 			<div className="mb-6">
+
+				
 				<label htmlFor="name">Name</label>
 				<input
 					type="text"
@@ -19,13 +21,6 @@ const Application = () => {
 				<select id="job-location" className="border border-green-500">
 					<option value="US"> United States</option>
 					<option value="UK"> United Kingdom</option>
-
-
-
-
-
-
-					
 
 					<option value="CA"> Canada</option>
 
